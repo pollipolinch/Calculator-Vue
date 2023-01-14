1c08f100ff221c86e83431097a293d571dc92dce
Полина <a1@MacBook-Air-Polina.local>
2023-01-14 20:08:58 +0500

    feat: add new function

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
 let vue = new Vue({
-    el: '#app',
-    data: {
-        result: '',
-        numbers:[1,2,3,4,5,6,7,8,9,0],
-        operations:['+','-','*','/'],
+  el: '#app',
+  data: {
+    previos: null,
+    result: '',
+    operator: null,
+    operatorClicked: false
+  },
+  methods: {
+    clear() {
+      this.result = ''
     },
-    methods:{
-      input(ell){
-        this.result = this.result.toString()
-        this.result = this.result+ell
-      },
-      reset(){
-        this.result = ''
-      },
-      calc(){
-        this.result=eval(this.result)
+    sign() {
+      if (this.result.charAt(0) === '-') {
+        this.result = this.result.slice(1)
+      } else {
+        this.result = `-${this.result}`
       }
+    },
+    percent() {
+      this.result = `${parseFloat(this.result) / 100}`
+    },
+    append(num) {
+      if(this.operatorClicked === true){
+        this.result = ''
+        this.operatorClicked = false
+      }
+      this.result = `${this.result}${num}`
+    },
+    dot() {
+      if (this.result.indexOf('.') === -1) {
+        this.append('.')
+      }
+    },
+    divide() {
+      this.operator = (a, b) => a / b
+      this.previos = this.result
+      this.operatorClicked = true
+    },
+    times() {
+      this.operator = (a,b)=> a*b
+      this.previos = this.result
+      this.operatorClicked = true
+    },
+    plus() {
+      this.operator = (a,b)=> a+b
+      this.previos = this.result
+      this.operatorClicked = true
+    },
+    minus() {
+      this.operator = (a,b)=> a-b
+      this.previos = this.result
+      this.operatorClicked = true
+    },
+    equal() {
+      this.result = `${this.operator(parseFloat(this.previos),parseFloat(this.result))}`
     }
+  }
 })
